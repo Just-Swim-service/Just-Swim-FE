b4703458235b6cbe3089296a5e73783a0b90cf25
jae-hwan-kim <woghks7859@gmail.com>
2024-03-24 23:06:31 +0900

    wip: login 디렉토리 구조 잡기

---
+++ just-swim-app/src/app/(afterLogin)/customer/page.tsx
@@ -0,0 +1,7 @@
+export default function Customer() {
+    return (
+        <div>
+            <h1>회원 홈</h1>
+        </div>
+    );
+}
---
+++ just-swim-app/src/app/(afterLogin)/instructor/page.tsx
@@ -0,0 +1,7 @@
+export default function Instructor() {
+    return (
+        <div>
+            <h1>강사 홈</h1>
+        </div>
+    );
+}
