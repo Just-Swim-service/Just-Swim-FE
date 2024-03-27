89caeb8ff88280433c4cbd52f663bf87cb8d036f
jae-hwan-kim <woghks7859@gmail.com>
2024-03-27 18:20:12 +0900

    refactor: 루트 layout Props 타입으로 수정

--- just-swim-app/src/app/layout.tsx
+++ just-swim-app/src/app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-    title: 'Just Swim',
-    description: 'VIP Swimming Feedback Service',
+  title: 'Just Swim',
+  description: 'VIP Swimming Feedback Service',
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html>
-            <body>{children}</body>
-        </html>
-    );
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<Props>) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
 }
