698d1abde486b581e51d4be67f3aaea7a4e6a384
jae-hwan-kim <woghks7859@gmail.com>
2024-03-31 19:47:17 +0900

    feat: 쿠키 또는 로컬스토리지 체크하는 함수 추가

--- just-swim-app/src/app/(beforeLogin)/login/page.tsx
+++ just-swim-app/src/app/(beforeLogin)/login/page.tsx
@@ -1,33 +1,72 @@
-"use client";
+'use client';
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 export default function Login() {
   const router = useRouter();
+  const type = '';
+
+  // 로컬스토리지 또는 쿠키 확인해서 이미 존재한다면 바로 페이지 이동하기
+  const checkCookie = () => {
+    console.log('쿠키 또는 로컬스토리지 확인');
+
+    // 쿠키에 접근하여 타입 확인
+    if (type) {
+      console.log('타입 존재');
+      return true;
+    } else {
+      console.log('타입 없음');
+      return false;
+    }
+  };
 
   const routeChooseType = () => {
     router.replace('/login/type');
   };
 
   const onClickGoogle = () => {
-    console.log('구글 로그인');
-    routeChooseType();
-  };
-  const onClickNaver = () => {
-    console.log('네이버 로그인');
-    routeChooseType();
-  };
-  const onClickKakao = () => {
-    console.log('Kakao 로그인');
-    routeChooseType();
+    if (checkCookie()) {
+      router.replace(`/${type}`);
+    } else {
+      console.log('구글 로그인');
+      routeChooseType();
+    }
   };
 
+  const onClickNaver = () => {
+    if (checkCookie()) {
+      router.replace(`/${type}`);
+    } else {
+      console.log('네이버 로그인');
+      routeChooseType();
+    }
+  };
+
+  const onClickKakao = () => {
+    if (checkCookie()) {
+      router.replace(`/${type}`);
+    } else {
+      console.log('카카오 로그인');
+      routeChooseType();
+    }
+  };
+
+  const onClickApple = () => {
+    if (checkCookie()) {
+      router.replace(`/${type}`);
+    } else {
+      console.log('애플 로그인');
+      routeChooseType();
+    }
+  };
+  
   return (
     <div>
       <h1 onClick={onClickGoogle}>구글 로그인</h1>
       <h1 onClick={onClickNaver}>네이버 로그인</h1>
       <h1 onClick={onClickKakao}>카카오톡 로그인</h1>
+      <h1 onClick={onClickApple}>애플 로그인</h1>
     </div>
   );
 }
