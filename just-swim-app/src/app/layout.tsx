87ab73950521b4dad9a087614be0633f52c15f91
jae-hwan-kim <woghks7859@gmail.com>
2024-03-31 19:20:53 +0900

    feat: 초기 스플래시 화면에 필요한 suspense 추가

--- just-swim-app/src/app/layout.tsx
+++ just-swim-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Just Swim',
@@ -13,7 +14,11 @@ type Props = {
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <Suspense fallback={<div>Loading...</div>}>
+        {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
