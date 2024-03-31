3c96549c556000653f238adc36268aece2d49ad6
jae-hwan-kim <woghks7859@gmail.com>
2024-03-31 19:45:50 +0900

    fix: 강사 & 회원 홈에서 필요없는 코드 삭제

--- just-swim-app/src/app/(afterLogin)/customer/page.tsx
+++ just-swim-app/src/app/(afterLogin)/customer/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-
 export default function Customer() {
-  const searchParams = useSearchParams();
-  const type = searchParams.get('type');
-
-  console.log('type : ', type);
-
   return (
     <div>
       <h1>회원 홈</h1>
--- just-swim-app/src/app/(afterLogin)/instructor/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-
 export default function Instructor() {
-  const searchParams = useSearchParams();
-  const type = searchParams.get('type');
-
-  console.log('type : ', type);
-
   return (
     <div>
       <h1>강사 홈</h1>
