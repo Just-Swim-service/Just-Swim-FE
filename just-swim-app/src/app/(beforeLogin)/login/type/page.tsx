0a4c2db69d6351876cc08786436039386b49a023
jae-hwan-kim <woghks7859@gmail.com>
2024-04-05 00:06:29 +0900

    feat: 1.3 회원구분 선택 페이지의 구분 전 레이아웃 구현

--- just-swim-app/src/app/(beforeLogin)/login/type/page.tsx
+++ just-swim-app/src/app/(beforeLogin)/login/type/page.tsx
@@ -1,16 +1,54 @@
-import Link from 'next/link';
+'use client';
 
-export default function Home() {
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import styles from './page.module.css';
+
+export default function Login() {
+  const router = useRouter();
+  const type = '';
+
   return (
-    <>
-      <main>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <h3>
+            원활한 서비스 이용을 위해
+            <br />
+            회원 타입을 선택해주세요.
+          </h3>
+        </div>
+        <div>
+          <p>이후 회원타입 변경이 불가능합니다.</p>
+        </div>
+      </div>
+      {/* TODO: 이미지 넣기 */}
+      <div className={styles.section}>
         <div>
           <Link
             href={{
               pathname: '/instructor',
               query: { type: 'instructor' },
             }}>
-            <h1>강사 버튼</h1>
+            <div className={styles.TypeButtonWrapper}>
+              <button className={styles.TypeButton}>
+                <div className={styles.TypeButtonImg}>
+                  <div></div>
+                </div>
+                <div className={styles.TypeButtonInfo}>
+                  <div>
+                    <h3>수영 강사</h3>
+                  </div>
+                  <div>
+                    <p>
+                      수영 강습 이력을
+                      <br />
+                      보유하신 분
+                    </p>
+                  </div>
+                </div>
+              </button>
+            </div>
           </Link>
         </div>
 
@@ -20,10 +58,33 @@ export default function Home() {
               pathname: '/customer',
               query: { type: 'customer' },
             }}>
-            <h1>회원 버튼</h1>
+            <div className={styles.TypeButtonWrapper}>
+              <button className={styles.TypeButton}>
+                <div className={styles.TypeButtonImg}>
+                  <div></div>
+                </div>
+                <div className={styles.TypeButtonInfo}>
+                  <div>
+                    <h3>수강생/보호자</h3>
+                  </div>
+                  <div>
+                    <p>
+                      수영 강습 수강생
+                      <br />
+                      혹은 보호자
+                    </p>
+                  </div>
+                </div>
+              </button>
+            </div>
           </Link>
         </div>
-      </main>
-    </>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.buttonWrapper}>
+          <button className={styles.SelectButton}>선택</button>
+        </div>
+      </div>
+    </div>
   );
 }
