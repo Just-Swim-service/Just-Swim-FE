03c94c0c7de7d091f9256bc206db1c3925b0a9fe
jae-hwan-kim <woghks7859@gmail.com>
2024-04-06 00:19:20 +0900

    feat: 이미지 추가

--- just-swim-app/src/app/(beforeLogin)/_component/onBoarding/OnBoardingSection.tsx
+++ just-swim-app/src/app/(beforeLogin)/_component/onBoarding/OnBoardingSection.tsx
@@ -1,4 +1,12 @@
 import styles from './OnBoarding.module.css';
+import Image from 'next/image';
+import Kakao from '@/../public/assets/kakao.svg';
+import Naver from '@/../public/assets/naver.svg';
+import Google from '@/../public/assets/google.svg';
+
+{
+  /* <Image src={LeftBtnSVG} alt="leftBtn" width={35} /> */
+}
 
 export default function OnBoardingSection({
   onClickKakao,
@@ -15,17 +23,26 @@ export default function OnBoardingSection({
       <div className={styles.section}>
         <div className={styles.buttonWrapper}>
           <button onClick={onClickKakao} className={styles.kakaoButton}>
-            카카오로 계속하기
+            <div>
+              <Image src={Kakao} alt="kakao" />
+              <p>카카오로 계속하기</p>
+            </div>
           </button>
         </div>
         <div className={styles.buttonWrapper}>
           <button onClick={onClickNaver} className={styles.naverButton}>
-            네이버로 계속하기
+            <div>
+              <Image src={Naver} alt="naver" />
+              <p>네이버로 계속하기</p>
+            </div>
           </button>
         </div>
         <div className={styles.buttonWrapper}>
           <button onClick={onClickGoogle} className={styles.googleButton}>
-            Google로 계속하기
+            <div>
+              <Image src={Google} alt="google" />
+              <p>Google로 계속하기</p>
+            </div>
           </button>
         </div>
       </div>
