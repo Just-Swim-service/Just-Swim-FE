1b9ad7cfb7dbfd415997fff956b13092398b3c24
jae-hwan-kim <woghks7859@gmail.com>
2024-04-13 16:40:52 +0900

    feat: bottom nav 동작 구현 및 레이아웃 분리

--- just-swim-app/src/app/(afterLogin)/instructor/layout.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/layout.tsx
@@ -1,9 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './Common.module.css';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [type, setType] = useState('schedule');
+
+  const handleBottomNavClick = (pageType: string) => {
+    console.log('Button clicked:', pageType);
+    setType(pageType);
+  };
+
   return (
-    <>
-      <h1>GNB</h1>
-      <h2>인사 문구</h2>
+    <div className={styles.container}>
       {children}
-    </>
+      <div className={styles.footer}>
+        {/* 네비게이션 바 */}
+        <div className={styles.bottomNavBar}>
+          <Link
+            href="/instructor/classmangement"
+            className={`${type === 'classmanagement' ? styles.isActive : ''}`}>
+            <button onClick={() => handleBottomNavClick('classmangement')}>
+              수업
+            </button>
+          </Link>
+          <Link
+            href="/instructor/schedule"
+            className={`${type === 'schedule' ? styles.isActive : ''}`}>
+            <button onClick={() => handleBottomNavClick('schedule')}>홈</button>
+          </Link>
+          <Link
+            href="/instructor/feedback"
+            className={`${type === 'feedback' ? styles.isActive : ''}`}>
+            <button onClick={() => handleBottomNavClick('feedback')}>
+              피드백
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
--- just-swim-app/src/app/(afterLogin)/instructor/schedule/layout.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/schedule/layout.tsx
@@ -1,10 +1,57 @@
-// export default function Layout({ children }: { children: React.ReactNode }) {
+'use client';
+
+import { useState } from 'react';
+import styles from './Common.module.css';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [type, setType] = useState('weekly');
+  const handleBottomNavClick = (buttonName: string) => {
+    console.log('Button clicked:', buttonName);
+    // 클릭된 버튼에 따른 동작 추가
+  };
+
   return (
-    <>
-      <h3>탭</h3>
-      {/* 탭 정보 넘기기 */}
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.topNavBar}>
+          <div>페이지 위치</div>
+          <button>설정</button>
+        </div>
+        <div className={styles.todayInfo}>
+          <p>13일 토요일, 오늘</p>
+          <h1>이후 등록된 수업이 없습니다.</h1>
+          {/* <div>예정된 수업이 2개 있습니다.</div> */}
+        </div>
+        <div className={styles.scheduleViewTypeTab}>
+          <div className={styles.TypeButtonWrapper}>
+            <button
+              className={`${styles.TypeButton} ${type === 'weekly' ? styles.isActive : ''}`}
+              onClick={() => setType('weekly')}>
+              <div className={styles.TypeButtonImg}>
+                <div></div>
+              </div>
+              <div className={styles.TypeButtonInfo}>
+                <div>
+                  <h3>주간</h3>
+                </div>
+              </div>
+            </button>
+          </div>
+          <button
+            className={`${styles.TypeButton} ${type === 'monthly' ? styles.isActive : ''}`}
+            onClick={() => setType('monthly')}>
+            <div className={styles.TypeButtonImg}>
+              <div></div>
+            </div>
+            <div className={styles.TypeButtonInfo}>
+              <div>
+                <h3>월간</h3>
+              </div>
+            </div>
+          </button>
+        </div>
+      </div>
       {children}
-    </>
+    </div>
   );
 }
