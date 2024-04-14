134d1df20d34494db0f35701e452f6c552d143c3
jae-hwan-kim <woghks7859@gmail.com>
2024-04-14 18:51:38 +0900

    fix: 오타 수정 및 className 위치 이동

--- just-swim-app/src/app/(afterLogin)/instructor/layout.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/layout.tsx
@@ -3,13 +3,11 @@
 import { useState } from 'react';
 import styles from './Common.module.css';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [type, setType] = useState('schedule');
+  const [type, setType] = useState('');
 
   const handleBottomNavClick = (pageType: string) => {
-    console.log('Button clicked:', pageType);
     setType(pageType);
   };
 
@@ -18,23 +16,25 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {children}
       <div className={styles.footer}>
         {/* 네비게이션 바 */}
-        <div className={styles.bottomNavBar}>
-          <Link
-            href="/instructor/classmangement"
-            className={`${type === 'classmanagement' ? styles.isActive : ''}`}>
-            <button onClick={() => handleBottomNavClick('classmangement')}>
+        <div className={styles.bottomNavBarWrapper}>
+          <Link href="/instructor/classManagement">
+            <button
+              className={`${type === 'classManagement' ? styles.isActive : ''}`}
+              onClick={() => handleBottomNavClick('classManagement')}>
               수업
             </button>
           </Link>
-          <Link
-            href="/instructor/schedule"
-            className={`${type === 'schedule' ? styles.isActive : ''}`}>
-            <button onClick={() => handleBottomNavClick('schedule')}>홈</button>
+          <Link href="/instructor/schedule">
+            <button
+              className={`${type === 'schedule' ? styles.isActive : ''}`}
+              onClick={() => handleBottomNavClick('schedule')}>
+              홈
+            </button>
           </Link>
-          <Link
-            href="/instructor/feedback"
-            className={`${type === 'feedback' ? styles.isActive : ''}`}>
-            <button onClick={() => handleBottomNavClick('feedback')}>
+          <Link href="/instructor/feedback">
+            <button
+              className={`${type === 'feedback' ? styles.isActive : ''}`}
+              onClick={() => handleBottomNavClick('feedback')}>
               피드백
             </button>
           </Link>
