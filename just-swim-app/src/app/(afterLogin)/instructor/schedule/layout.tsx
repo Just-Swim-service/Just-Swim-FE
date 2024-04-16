2abb8aaad634756eedbf8a9279b6d52f176ec080
jae-hwan-kim <woghks7859@gmail.com>
2024-04-16 21:03:58 +0900

    feat: 합치기 위한 커밋

--- just-swim-app/src/app/(afterLogin)/instructor/schedule/layout.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/schedule/layout.tsx
@@ -3,6 +3,10 @@
 import { useState } from 'react';
 import styles from './Common.module.css';
 
+import Weekly from '/public/assets/weekly.svg';
+import Monthly from '/public/assets/monthly.svg';
+import Image from 'next/image';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [type, setType] = useState('weekly');
 
@@ -12,14 +16,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className={styles.topNavBarWrapper}>
           <div>페이지 위치</div>
           <button>
-            <div>
-              사진
-            </div>
+            <div>사진</div>
           </button>
         </div>
         <div className={styles.todayInfo}>
+          {/* 날짜로 데이터 바꿔줘야함 */}
           <p>13일 토요일, 오늘</p>
-          <h1>이후 등록된 수업이 없습니다.</h1>
+          <h3>이후 등록된 수업이 없습니다.</h3>
           {/* <div>예정된 수업이 2개 있습니다.</div> */}
         </div>
         <div className={styles.scheduleViewTypeTab}>
@@ -32,23 +35,26 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               </div>
               <div className={styles.TypeButtonInfo}>
                 <div>
+                  {/* TODO: Path 에 접근해서 색 조정하기 */}
+                  <Image className={styles.yaho} src={Weekly} alt="주간" />
                   <h3>주간</h3>
                 </div>
               </div>
             </button>
-          </div>
-          <button
-            className={`${styles.TypeButton} ${type === 'monthly' ? styles.isActive : ''}`}
-            onClick={() => setType('monthly')}>
-            <div className={styles.TypeButtonImg}>
-              <div></div>
-            </div>
-            <div className={styles.TypeButtonInfo}>
-              <div>
-                <h3>월간</h3>
+            <button
+              className={`${styles.TypeButton} ${type === 'monthly' ? styles.isActive : ''}`}
+              onClick={() => setType('monthly')}>
+              <div className={styles.TypeButtonImg}>
+                <div></div>
               </div>
-            </div>
-          </button>
+              <div className={styles.TypeButtonInfo}>
+                <div>
+                  <Image src={Monthly} alt="월관" />
+                  <h3>월간</h3>
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
       <div className={styles.section}>
