b7bf445942fcf2f19385fc6d82db38c3a3844ba6
yeji-git <ye-end@daum.net>
2024-04-20 09:32:09 +0900

    design: searchBox UI 수정

--- just-swim-app/src/app/(afterLogin)/instructor/classList/classListTabs/_components/searchBox/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/classList/classListTabs/_components/searchBox/page.tsx
@@ -1,27 +1,14 @@
-import styles from './searchBox.module.css';
+import Image from 'next/image';
+import searchIcon from '/public/assets/icon_search.png';
+import './searchBox.scss';
 
 export default function SearchBox() {
   return (
     <>
-      <form action="" className={styles.searchForm}>
-        <input
-          placeholder="회원명으로 검색하세요"
-          className={styles.searchInput}
-        />
-        <button type="submit" className={styles.searchButton}>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
-              fill="#5C5E62"
-            />
-          </svg>
-        </button>
-      </form>
+      <div className="search">
+        <Image src={searchIcon} alt="검색" />
+        <input type="text" placeholder="수강생 검색하기" />
+      </div>
     </>
   );
 }
