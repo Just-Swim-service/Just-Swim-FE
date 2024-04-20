b7d3d6d2560c77e4c76772603eac5d9fb66c869e
yeji-git <ye-end@daum.net>
2024-04-20 09:34:00 +0900

    design: 수업 관리 - 수업 목록 및 수강생 목록 - 수강생 상세 정보 UI 수정

--- just-swim-app/src/app/(afterLogin)/instructor/classList/customerDetail/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/classList/customerDetail/page.tsx
@@ -2,11 +2,13 @@
 
 import CustomerInfo from './_components/customerInfo/page';
 import FeedbackRecord from './_components/feedbackRecord/page';
+import CustomerClass from './_components/customerClass/page';
 
 export default function customerDetail() {
   return (
     <>
       <CustomerInfo />
+      <CustomerClass />
       <FeedbackRecord />
     </>
   );
