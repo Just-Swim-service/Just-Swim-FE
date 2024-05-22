c55481cb37e40d9be74ca35bf32674f7b25d65a6
khv2644511 <khv2644511@naver.com>
2024-05-22 17:19:30 +0900

    feat: Header 뒤로가기 기능 추가

--- just-swim-app/src/app/_component/Header.tsx
+++ just-swim-app/src/app/_component/Header.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import Image from 'next/image';
 import arrowBackIcon from '/public/assets/icon_arrow_back.png';
 
 import './header.scss';
+import { useRouter } from 'next/navigation'
 
 interface Props {
   title: string;
 }
+
 export default function Header({ title }: Props) {
+  const router = useRouter()
+
+  const goBack = () => {
+    router.back()
+  }
+
   return (
     <header>
-      <Image src={arrowBackIcon} alt="뒤로가기" />
+      <Image src={arrowBackIcon} alt="뒤로가기" onClick={()=> goBack()} />
       <div>{title}</div>
     </header>
   );
