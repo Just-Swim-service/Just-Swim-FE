4d465508fed133c29e1dc8c2e9f2a8bf7e49976a
khv2644511 <khv2644511@naver.com>
2024-06-05 11:19:39 +0900

    fix: Profile 컴포넌트 count prop 추가로 인원수 표시하기

--- just-swim-app/src/app/(afterLogin)/_component/Profile.tsx
+++ just-swim-app/src/app/(afterLogin)/_component/Profile.tsx
@@ -6,37 +6,48 @@ interface Props {
   width: number;
   height: number;
   xMargin: number;
+  count: boolean;
 }
 export default function Profile({
   customers,
   width,
   height,
   xMargin = -5,
+  count,
 }: Props) {
   return (
-    <div className={styled.img_list}>
-      {/* <Image src={`/assets/profile1.png`} alt="프로필" width={40} height={40} /> */}
-      {customers.map((el, index) => {
-        return el.profile ? (
-          <Image
-            key={index}
-            src={`/assets/${el.profile}.png`}
-            alt="프로필"
-            width={width}
-            height={height}
-            style={{ margin: `0px ${xMargin}px` }}
-          />
-        ) : (
-          <Image
-            key={index}
-            src={`/assets/no_profile.png`}
-            alt="프로필"
-            width={width}
-            height={height}
-            style={{ margin: `0px ${xMargin}px` }}
-          />
-        );
-      })}
+    <div className={styled.row}>
+      <div className={styled.img_list}>
+        {/* <Image src={`/assets/profile1.png`} alt="프로필" width={40} height={40} /> */}
+        {customers.map((el, index) => {
+          return el.profile ? (
+            <Image
+              key={index}
+              src={`/assets/${el.profile}.png`}
+              alt="프로필"
+              width={width}
+              height={height}
+              style={{ margin: `0px ${xMargin}px` }}
+            />
+          ) : (
+            <Image
+              key={index}
+              src={`/assets/no_profile.png`}
+              alt="프로필"
+              width={width}
+              height={height}
+              style={{ margin: `0px ${xMargin}px` }}
+            />
+          );
+        })}
+      </div>
+      {count ? (
+        <div className={styled.count} style={{ marginLeft: '9px' }}>
+          {customers.length}명
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
