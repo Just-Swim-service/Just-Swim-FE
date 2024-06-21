602021dcd776a01fe82455e854ea6561b8252b0e
sdh20282 <sdh20282@gmail.com>
2024-06-21 10:16:12 +0900

    refactor: Color Input component JSDoc 변경

--- just-swim-app/src/app/(afterLogin)/test/page.tsx
+++ just-swim-app/src/app/(afterLogin)/test/page.tsx
@@ -20,15 +20,13 @@ export default function Test() {
       position: 'relative'
     }}>
       <form action="" className={styled.form}>
-        <TextInput name="textinput" placeholder="수업명을 입력해주세요" valid={false} />
+        <TextInput name="textinput" placeholder="수업명을 입력해주세요" valid={true} />
         <TextArea name="textarea" placeholder="피드백을 입력해주세요" height={100} />
         <LocationInput name="locationinput" placeholder="수업 위치를 선택해 주세요" valid={false} />
         <ColorInput name="colorinput" />
         <TimeInput name="timeinput" />
         <FormButton text="확인" active={true} />
       </form>
-        
-      {/* <Datepicker /> */}
       {/* <ConfirmButton text="버튼 1" kind="confirm" active={true} />
       <ConfirmButton text="버튼 2" kind="confirm-sub" border={false} />
       <ConfirmButton text="버튼 3" kind="cancel" />
--- just-swim-app/src/app/_components/form/colorInput/index.tsx
+++ just-swim-app/src/app/_components/form/colorInput/index.tsx
@@ -62,7 +62,7 @@ ref: ForwardedRef<HTMLInputElement>) {
 }
 
 /**
- * 상위 컴포넌트에서 TextInput 대한 className을 직접 설정하지 않도록 주의! (동작하지 않음)
+ * 상위 컴포넌트에서 ColorInput 대한 className을 직접 설정하지 않도록 주의! (동작하지 않음)
  * @param {string} name input의 name
  * @param {import('react').MutableRefObject<HTMLInputElement>} ref input의 ref attribute에 연결할 target
  * @param {import('react').InputHTMLAttributes<HTMLInputElement>} attributes input에서 사용 가능한 모든 attributes
