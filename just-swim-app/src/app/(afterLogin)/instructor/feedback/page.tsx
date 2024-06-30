aec21d4ed6f45ac78202e683308dea358ccf0474
khv2644511 <khv2644511@naver.com>
2024-06-30 17:32:47 +0900

    file: url 변경으로 feedbackView 폴더 삭제

--- just-swim-app/src/app/(afterLogin)/instructor/feedback/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/feedback/page.tsx
@@ -1,5 +1,25 @@
-export default function Feedback() {
+'use client';
+
+import { ProfileHeader } from '@components';
+
+import { FeedbackList, FeedbackTypeButton } from './_components';
+import styled from './feedback.module.scss';
+// import { Get } from '@/_apis/feedback/route';
+
+export default async function FeedbackView() {
+  // const FeedbackList = await Get();
+  // console.log(FeedbackList);
+
+  const data = {
+    name: '강사',
+    image: '',
+  };
+
   return (
-    <h1>Feedback</h1>
+    <div className={styled.layout}>
+      <ProfileHeader leftContent="피드백" data={data} />
+      <FeedbackTypeButton />
+      <FeedbackList />
+    </div>
   );
 }
