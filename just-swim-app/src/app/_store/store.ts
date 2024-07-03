0b03d47b74ec6fbc44b89c17a34d76eef5b45d8b
khv2644511 <khv2644511@naver.com>
2024-07-03 10:40:50 +0900

    fix: 새로고침 시 선택한 데이터 초기화되는 문제 해결

--- just-swim-app/src/app/_store/store.ts
+++ just-swim-app/src/app/_store/store.ts
@@ -1,59 +1,84 @@
 import { ChangeEvent } from 'react';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface Member {
-  "memberId": string;
-  "userId": string;
-  "memberNickname": string;
-  "profileImage": string;
+  memberId: string;
+  userId: string;
+  memberNickname: string;
+  profileImage: string;
 }
 
 type State = {
-  userList : Member[]
-  checkedList: Member[]
-}
+  userList: Member[];
+  checkedList: Member[];
+};
 
 type Prams = {
-  userId : string
-  checkedList: Member[]
-}
+  userId: string;
+  checkedList: Member[];
+};
 
 type Action = {
-  checkItemHandler: (e : ChangeEvent<HTMLInputElement>, userId: Prams['userId']) => void
-  removeItemHandler: (userId: Prams['userId']) => void
-}
+  checkItemHandler: (
+    e: ChangeEvent<HTMLInputElement>,
+    userId: Prams['userId'],
+  ) => void;
+  removeItemHandler: (userId: Prams['userId']) => void;
+};
 
-const searchUserStore = create<State & Action>((set) => ({
-  userList: [
+const searchUserStore = create(
+  persist<State & Action>(
+    (set) => ({
+      userList: [
+        {
+          memberId: '1',
+          userId: '1',
+          memberNickname: '홍길동',
+          profileImage:
+            'http://k.kakaocdn.net/dn/d3UHmi/btsH8xClKxG/jGQI0gBeKrlOkneK7KYIbK/img_640x640.jpg',
+        },
+        {
+          memberId: '2',
+          userId: '10',
+          memberNickname: '홍길순',
+          profileImage:
+            'http://k.kakaocdn.net/dn/d3UHmi/btsH8xClKxG/jGQI0gBeKrlOkneK7KYIbK/img_640x640.jpg',
+        },
+      ],
+      checkedList: [],
+      checkItemHandler: (e: ChangeEvent<HTMLInputElement>, userId: string) =>
+        set((state: any) => {
+          const isChecked = e.target.checked;
+          console.log(isChecked);
+          console.log(state.checkedList);
+
+          const selectMember = state.userList.find(
+            (member: Member) => member.userId === userId,
+          );
+          if (!selectMember) return state;
+          return {
+            checkedList: isChecked
+              ? [...state.checkedList, selectMember]
+              : state.checkedList.filter(
+                  (member: Member) => member.userId !== userId,
+                ),
+          };
+        }),
+      removeItemHandler: (userId: string) =>
+        set((state: any) => ({
+          checkedList: state.checkedList.filter(
+            (member: Member) => member.userId !== userId,
+          ),
+        })),
+
+      setCheckedList: (state: any) => set({ checkedList: state.checkedList }),
+    }),
     {
-      "memberId": "1",
-      "userId": "1",
-      "memberNickname": "홍길동",
-      "profileImage": "http://k.kakaocdn.net/dn/d3UHmi/btsH8xClKxG/jGQI0gBeKrlOkneK7KYIbK/img_640x640.jpg"
+      name: 'checked_list',
+      partialize: (state: any) => ({ checkedList: state.checkedList }),
     },
-    {
-      "memberId": "2",
-      "userId": "10",
-      "memberNickname": "홍길순",
-      "profileImage": "http://k.kakaocdn.net/dn/d3UHmi/btsH8xClKxG/jGQI0gBeKrlOkneK7KYIbK/img_640x640.jpg"
-    }
-  ],
-  checkedList: [],
-  checkItemHandler: (e: ChangeEvent<HTMLInputElement>, userId: string) => set((state: any) => {
-    const isChecked = e.target.checked;
-    console.log(isChecked)
-    const selectMember = state.userList.find((member:Member) => member.userId === userId);
-    if (!selectMember) return state;
+  ),
+);
 
-    return {
-      checkedList: isChecked
-        ? [...state.checkedList, selectMember]
-        : state.checkedList.filter((member: Member) => member.userId !== userId),
-    };
-  }),
-  removeItemHandler: (userId: string) => set((state: any) => ({
-    checkedList: state.checkedList.filter((member:Member) => member.userId !== userId),
-  }))
-}))
-
-export { searchUserStore};
+export { searchUserStore };
