425681e5f95eb7834d325fd2da0f1038571e4fcb
khv2644511 <khv2644511@naver.com>
2024-07-06 23:40:20 +0900

    feat: feedback 페이지 nav bar 추가

--- just-swim-app/src/app/(afterLogin)/instructor/feedback/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/feedback/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { ProfileHeader } from '@components';
+import { InstructorBottomNavBar, ProfileHeader } from '@components';
 
 import { FeedbackList, FeedbackTypeButton } from './_components';
 import styled from './feedback.module.scss';
-// import { Get } from '@/_apis/feedback/route';
 
-export default async function FeedbackView() {
-  // const FeedbackList = await Get();
-  // console.log(FeedbackList);
-
+export default async function Feedback() {
   const data = {
     name: '강사',
     image: '',
@@ -20,6 +16,9 @@ export default async function FeedbackView() {
       <ProfileHeader leftContent="피드백" data={data} />
       <FeedbackTypeButton />
       <FeedbackList />
+      <div className={styled.nav}>
+        <InstructorBottomNavBar />
+      </div>
     </div>
   );
 }
