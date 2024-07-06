90fb26d6019bd53271ac2ce4ea06576a76ef1a16
khv2644511 <khv2644511@naver.com>
2024-07-07 00:21:58 +0900

    fix: 주석 제거

--- just-swim-app/src/app/(afterLogin)/instructor/feedback/create/person/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/feedback/create/person/page.tsx
@@ -22,15 +22,6 @@ import {
 import { IconCalendar } from '@assets';
 
 export default function FeedbackWrite() {
-  let now = new Date();
-
-  let yaer = now.getFullYear();
-  let month = (now.getMonth() + 1).toString().padStart(2, '0');
-  let day = now.getDate().toString().padStart(2, '0');
-
-  let date = `${yaer}.${month}.${day}`;
-  // console.log(date)
-
   const fileRef = useRef<HTMLInputElement>(null);
   const handleClick = () => {
     console.log('fileRef', fileRef);
