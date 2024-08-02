e4ae64cb4fe11b941c272a41ef23010997c3b1d7
박예지 <110365183+yeji-git@users.noreply.github.com>
2024-08-02 17:09:39 +0900

    feature: 수업 세부 정보 확인 API 추가

--- just-swim-app/src/app/(afterLogin)/instructor/class/detail/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/class/detail/[id]/page.tsx
@@ -1,181 +1,221 @@
 'use client';
 
 import Image from 'next/image';
-import profile from '/public/assets/profile1.png';
-import QRCode from '/public/assets/qr_code.png';
-import downloadIcon from '/public/assets/icon_download.png';
-import shareIcon from '/public/assets/icon_share.png';
-import arrowRightIcon from '/public/assets/icon_arrow_right.png';
-import deleteIcon from '/public/assets/icon_delete.png';
 import styled from './classDetail.module.scss';
-import location_icon from '/public/assets/input_icon_location.png';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-
-import { Profile, ClassInfo, Header, DatepickerPrev, TimepickerPrev, RepeatDatepicker } from '@components';
-
-// 수업 참여자 리스트
-let peopleList = [
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-  {
-    name: 'hyebin',
-    profile: '',
-  },
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-  {
-    name: 'hyebin',
-    profile: 'profile1',
-  },
-];
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import {
+  IconArrowRight,
+  IconTrashcan,
+  IconClock,
+  IconCalendar,
+  IconLocation,
+  IconRepeat,
+  IconShare,
+  IconDownload,
+} from '@assets';
+import { Header } from '@components';
+import QRCode from '/public/assets/qr_code.png';
 
 export default function ClassDetail() {
-  const router = useRouter();
+  const params = useParams();
 
-  const [showModal, setShowModal] = useState(false);
-  // const clickModal = () => setShowModal(!showModal);
-  const clickModal = () => {
-    setShowModal((prev) => !prev);
-  };
+  const lectureId = params.id;
+  const API_URL = `${process.env.NEXT_PUBLIC_DB_HOST}/api/lecture/${lectureId}`;
+  const AUTHORIZATION_HEADER = `${process.env.NEXT_PUBLIC_DB_TOKEN}`;
+
+  const [lecture, setLecture] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: AUTHORIZATION_HEADER,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`API error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setLecture(data.data);
+      })
+      .catch((error) => {
+        console.error('API error:', error);
+        setError(error);
+      });
+  }, []);
+
+  const lectureData = lecture.map((item) => ({
+    lectureId: item.lectureId,
+    lectureTitle: item.lectureTitle,
+    lectureContent: item.lectureContent,
+    lectureTime: item.lectureTime ? item.lectureTime.split('-') : [],
+    lectureDays: item.lectureDays.replace(/./g, '$& ').trim(),
+    lectureLocation: item.lectureLocation,
+    lectureColor: item.lectureColor,
+    lectureQRCode: item.lectureQRCode,
+    lectureEndDate: item.lectureEndDate,
+    instructorName: item.instructorName,
+    instructorProfileImage: item.instructorProfileImage,
+    memberUserId: item.memberUserId,
+    memberProfileImage: item.memberProfileImage,
+  }));
+
+  if (error) {
+    console.error('API error:', error);
+    return <p>API 오류 발생</p>;
+  }
+
+  if (!lecture || lecture.length === 0) {
+    return <p>데이터 로딩 중...</p>;
+  }
 
   return (
-    <div className={styled.class_detail}>
+    <div>
       <Header
         title="수업 정보"
-        editURL="/instructor/classList/classDetail/edit"
+        editURL={`/instructor/class/edit/${lectureId}`}
       />
 
-      <div className={styled.qr}>
-        <h2>아침 5반</h2>
-        <div className={styled.desc}>
-          초보반으로, 배영 및 접영 위주로 수업합니다.
-        </div>
+      {lectureData.map((item) => (
+        <>
+          <div className={styled.qr}>
+            <h2>{item.lectureTitle}</h2>
+            <div className={styled.desc}>{item.lectureTitle}</div>
 
-        <div className={styled.instructor}>
-          <Image src={profile} alt="프로필" width={24} height={24} />
-          <div>홍길동 강사님 수업</div>
-        </div>
-
-        <Image className={styled.qr_image} src={QRCode} alt="qr" />
-
-        <div className={styled.save_share}>
-          <div className={styled.col}>
-            <div className={styled.circle}>
-              <Image src={downloadIcon} alt="저장" />
+            <div className={styled.instructor}>
+              <Image
+                src={item.instructorProfileImage}
+                alt="프로필"
+                width={24}
+                height={24}
+                style={{ borderRadius: '50% 50%', marginRight: '7px' }}
+              />
+              <div>{item.instructorName} 강사님 수업</div>
             </div>
-            <div>저장하기</div>
-          </div>
 
-          <div className={styled.col}>
-            <div className={styled.circle}>
-              <Image src={shareIcon} alt="공유" />
-            </div>
-            <div>공유하기</div>
-          </div>
-        </div>
-      </div>
+            <Image className={styled.qr_image} src={QRCode} alt="qr" />
 
-      <div className={styled.invite}>
-        <h3>초대된 수강생</h3>
-        <div className={styled.student}>
-          <div className={`${styled.count} ${styled.box}`}>
-            <div>현재 인원</div>
-            <div>{'15명'}</div>
-          </div>
-          <div className={`${styled.profile} ${styled.box}`}>
-            {peopleList.length > 0 ? (
-              <>
-                <Profile
-                  customers={peopleList}
-                  width={32}
-                  height={32}
-                  xMargin={-5}
-                />
-                <Image
-                  src={arrowRightIcon}
-                  alt="arrow right"
-                  width={20}
-                  height={20}
-                />
-              </>
-            ) : (
-              <div className={styled.profile_no}>
-                QR(수업 정보)을 공유해주세요!
+            <div className={styled.save_share}>
+              <div className={styled.col}>
+                <div className={styled.circle}>
+                  <IconDownload />
+                </div>
+                <div>저장하기</div>
               </div>
+              <div className={styled.col}>
+                <div className={styled.circle}>
+                  <IconShare />
+                </div>
+                <div>공유하기</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styled.invite}>
+            <h3>수강생 목록</h3>
+            <div className={styled.student}>
+              <div className={styled.count_box}>
+                <p className={styled.personnel}>현재 인원</p>
+                <p className={styled.count}>
+                  {item.memberUserId == 0 ? `0명` : `1명`}
+                </p>
+              </div>
+              <div className={`${styled.profile} ${styled.box}`}>
+                {item.memberUserId == 0 ? (
+                  <div className={styled.profile_no}>
+                    QR(수업 정보)을 공유해주세요!
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className={styled.class}>
+            <h3>수업 정보</h3>
+
+            <div className={styled.class_info}>
+              <div className={styled.class_time}>
+                <div>
+                  <span className={styled.icon}>
+                    <IconClock />
+                  </span>
+                  <span className={styled.twelve}>
+                    {parseInt(item.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
+                  </span>
+                  {item.lectureTime[0]}
+                </div>
+                ~
+                <div>
+                  <span className={styled.twelve}>
+                    {parseInt(item.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
+                  </span>
+                  {item.lectureTime[1]}
+                </div>
+              </div>
+
+              <div className={styled.lecture_info}>
+                <p>
+                  <span className={styled.icon}>
+                    <IconCalendar />
+                  </span>
+                  매주&nbsp;
+                  {item.lectureDays}요일
+                </p>
+              </div>
+
+              <div className={styled.lecture_info}>
+                <p>
+                  <span className={styled.icon}>
+                    <IconLocation />
+                  </span>
+                  {item.lectureLocation}
+                </p>
+              </div>
+
+              <div className={styled.lecture_info}>
+                <p>
+                  <span className={styled.icon}>
+                    <IconRepeat />
+                  </span>
+                  종료일 없이 반복
+                </p>
+              </div>
+
+              <div className={styled.lecture_info}>
+                <p>
+                  <span
+                    className={styled.color_box}
+                    style={{ backgroundColor: `${item.lectureColor}` }}></span>
+                  구분색
+                </p>
+              </div>
+
+              <div className={styled.color_input}></div>
+            </div>
+
+            <button className={styled.delete}>
+              <IconTrashcan />
+              <div>수업 삭제</div>
+            </button>
+
+            {item.memberUserId == 0 ? (
+              <></>
+            ) : (
+              <button className={styled.feedback_btn}>
+                수강생 전체 피드백 남기기
+              </button>
             )}
           </div>
-        </div>
-      </div>
-
-      <div className={styled.class}>
-        <h3>수업 정보</h3>
-        {/* <ClassInfo islabel={false} bgColor="white" /> */}
-
-        <div className={styled.classInfo}>
-          <div className={styled.classInfo_time}>
-            <TimepickerPrev label="시작" />
-            ~
-            <TimepickerPrev label="끝" />
-          </div>
-          {/* hyebin 매주 요일 지정 구현하기 */}
-
-          <DatepickerPrev />
-
-          <div className={styled.location_input}>
-            <input
-              id="location"
-              className={`${styled.input} ${styled.white}`}
-              placeholder="강동구 실내 수영장"
-              onClick={() => router.push('edit/search')}
-            />
-            <Image
-              src={location_icon}
-              alt="location icon"
-              width={20}
-              height={20}
-              className={styled.locatin_icon}
-            />
-          </div>
-
-          <RepeatDatepicker />
-
-          <div className={styled.color_input}>
-            <input
-              className={`${styled.input} ${styled.white}`}
-              onClick={clickModal}
-              placeholder="구분색"></input>
-            <div className={styled.pick_color} />
-          </div>
-          {/* {showModal && (
-            <ColorModal showModal={showModal} setShowModal={setShowModal} />
-          )} */}
-        </div>
-
-        <button className={styled.delete}>
-          <Image src={deleteIcon} alt="수업 삭제" width={24} height={24} />
-          <div>수업 삭제</div>
-        </button>
-        <button className={styled.feedback_btn}>
-          수강생 전체 피드백 남기기
-        </button>
-      </div>
+        </>
+      ))}
     </div>
   );
 }
