fcb01ade7d39c5a0159a85fbcb4ada2877391345
sdh20282 <sdh20282@gmail.com>
2024-08-09 05:54:47 +0900

    refactor: hideModal props 타입을 HTMLButtonElement -> HTMLElement로 변경

--- just-swim-app/src/app/_types/typeModal.ts
+++ just-swim-app/src/app/_types/typeModal.ts
@@ -4,24 +4,24 @@ import { DayProps } from "@types";
 
 export interface ColorModalProps {
   initialColor: string,
-  hideModal: (event: MouseEvent<HTMLButtonElement>) => void,
+  hideModal: (event: MouseEvent<HTMLElement>) => void,
   setColor: (color: string) => void,
 }
 
 export interface DateModalProps {
   initialDate: string,
-  hideModal: (event: MouseEvent<HTMLButtonElement>) => void,
+  hideModal: (event: MouseEvent<HTMLElement>) => void,
   setDate: (color: string) => void,
 }
 
 export interface DayModalProps {
   initialDays: DayProps,
-  hideModal: (event: MouseEvent<HTMLButtonElement>) => void,
+  hideModal: (event: MouseEvent<HTMLElement>) => void,
   setDays: (days: DayProps) => void,
 }
 
 export interface TimeModalProps {
   timeValue: string,
-  hideModal: (event: MouseEvent<HTMLButtonElement>) => void,
+  hideModal: (event: MouseEvent<HTMLElement>) => void,
   setTimeValue: (time: string) => void,
 }
