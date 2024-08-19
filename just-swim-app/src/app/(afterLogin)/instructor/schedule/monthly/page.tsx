cfe015b3018e57918ac77643d34b84ae0f9f569f
sdh20282 <sdh20282@gmail.com>
2024-08-19 12:04:48 +0900

    refactor: 기존 schedule 코드 삭제

--- just-swim-app/src/app/(afterLogin)/instructor/schedule/monthly/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Monthly() {
-  redirect("/instructor/schedule/monthly/classList");
-}
--- just-swim-app/src/app/(afterLogin)/instructor/schedule/weekly/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Weekly() {
-  redirect("/instructor/schedule/weekly/classList");
-}
