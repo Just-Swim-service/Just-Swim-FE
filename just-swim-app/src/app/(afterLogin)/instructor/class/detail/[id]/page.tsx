efa1f4a9d54e3dd257fba3396b909a14f0c4469d
박예지 <110365183+yeji-git@users.noreply.github.com>
2024-08-22 09:27:22 +0900

    fix: DB 구조 변경으로 인한 수정

--- just-swim-app/src/app/(afterLogin)/instructor/class/detail/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/class/detail/[id]/page.tsx
@@ -14,7 +14,6 @@ import {
   IconDownload,
 } from '@assets';
 import { Header } from '@components';
-import QRCode from '/public/assets/qr_code.png';
 import { useRouter } from 'next/navigation';
 
 interface ConfirmModalProps {
@@ -65,8 +64,7 @@ export default function ClassDetail() {
   const API_URL = `${process.env.NEXT_PUBLIC_DB_HOST}/api/lecture/${lectureId}`;
   const AUTHORIZATION_HEADER = `${process.env.NEXT_PUBLIC_DB_TOKEN}`;
 
-  const [lecture, setLecture] = useState([]);
-  const [error, setError] = useState(null);
+  const [lecture, setLecture] = useState();
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   useEffect(() => {
@@ -76,45 +74,20 @@ export default function ClassDetail() {
         Authorization: AUTHORIZATION_HEADER,
       },
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`API error: ${response.status}`);
-        }
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setLecture(data.data);
-      })
-      .catch((error) => {
-        console.error('API error:', error);
-        setError(error);
+        if (data.data && data.data.lectureTime) {
+          const lectureTime = data.data.lectureTime.split('-');
+          setLecture({ ...data.data, lectureTime });
+          console.log(data);
+        } else {
+          setLecture(data.data);
+        }
       });
-  }, []);
-
-  const lectureData = lecture.map((item) => ({
-    lectureId: item.lectureId,
-    lectureTitle: item.lectureTitle,
-    lectureContent: item.lectureContent,
-    lectureTime: item.lectureTime ? item.lectureTime.split('-') : [],
-    lectureDays: item.lectureDays.replace(/(.)(?=.)/g, '$1, '),
-    lectureLocation: item.lectureLocation,
-    lectureColor: item.lectureColor,
-    lectureQRCode: item.lectureQRCode,
-    lectureEndDate: item.lectureEndDate,
-    instructorName: item.instructorName,
-    instructorProfileImage: item.instructorProfileImage,
-    memberUserId: item.memberUserId,
-    memberProfileImage: item.memberProfileImage,
-  }));
-
-  if (error) {
-    console.error('API error:', error);
-    return <p>API 오류가 발생하였습니다.</p>;
-  }
+  }, [lectureId]);
 
   if (!lecture || lecture.length === 0) {
-    return <p>데이터를 로딩 중입니다...</p>;
+    return <p>로딩 중</p>;
   }
 
   const DeleteHandler = async () => {
@@ -147,156 +120,174 @@ export default function ClassDetail() {
         editURL={`/instructor/class/edit/${lectureId}`}
       />
 
-      {lectureData.map((item) => (
-        <>
-          <div className={styled.qr}>
-            <h2>{item.lectureTitle}</h2>
-            <div className={styled.desc}>{item.lectureTitle}</div>
+      <div className={styled.qr}>
+        <h2>{lecture.lectureTitle}</h2>
+        <div className={styled.desc}>{lecture.lectureTitle}</div>
 
-            <div className={styled.instructor}>
-              <Image
-                src={item.instructorProfileImage}
-                alt="프로필"
-                width={24}
-                height={24}
-                style={{ borderRadius: '50% 50%', marginRight: '7px' }}
-              />
-              <div>{item.instructorName} 강사님 수업</div>
+        <div className={styled.instructor}>
+          <Image
+            src={lecture.instructor.instructorProfileImage}
+            alt="프로필"
+            width={24}
+            height={24}
+            style={{ borderRadius: '50% 50%', marginRight: '7px' }}
+          />
+          <div>{lecture.instructor.instructorName} 강사님 수업</div>
+        </div>
+
+        <Image
+          className={styled.qr_image}
+          src={`/qrcode/${lectureId}`}
+          alt="QR code"
+          width={114}
+          height={114}
+        />
+
+        <div className={styled.save_share}>
+          <div className={styled.col}>
+            <div className={styled.circle}>
+              <IconDownload />
             </div>
-
-            <Image className={styled.qr_image} src={QRCode} alt="qr" />
-
-            <div className={styled.save_share}>
-              <div className={styled.col}>
-                <div className={styled.circle}>
-                  <IconDownload />
-                </div>
-                <div>저장하기</div>
-              </div>
-              <div className={styled.col}>
-                <div className={styled.circle}>
-                  <IconShare />
-                </div>
-                <div>공유하기</div>
-              </div>
-            </div>
+            <div>저장하기</div>
           </div>
-
-          <div className={styled.invite}>
-            <h3>수강생 목록</h3>
-            <div className={styled.student}>
-              <div className={styled.count_box}>
-                <p className={styled.personnel}>현재 인원</p>
-                <p className={styled.count}>
-                  {item.memberUserId && typeof item.memberUserId === 'number'
-                    ? `${item.memberUserId}명`
-                    : '0명'}
-                </p>
-              </div>
-              <div className={`${styled.profile} ${styled.box}`}>
-                {item.memberUserId === null ? (
-                  <div className={styled.profile_no}>
-                    QR(수업 정보)을 공유해주세요!
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
+          <div className={styled.col}>
+            <div className={styled.circle}>
+              <IconShare />
             </div>
+            <div>공유하기</div>
           </div>
+        </div>
+      </div>
 
-          <div className={styled.class}>
-            <h3>수업 정보</h3>
-
-            <div className={styled.class_info}>
-              <div className={styled.class_time}>
-                <div>
-                  <span className={styled.icon}>
-                    <IconClock />
-                  </span>
-                  <span className={styled.twelve}>
-                    {parseInt(item.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
-                  </span>
-                  {item.lectureTime[0]}
-                </div>
-                ~
-                <div>
-                  <span className={styled.twelve}>
-                    {parseInt(item.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
-                  </span>
-                  {item.lectureTime[1]}
-                </div>
-              </div>
-
-              <div className={styled.lecture_info}>
-                <p>
-                  <span className={styled.icon}>
-                    <IconCalendar />
-                  </span>
-                  매주&nbsp;
-                  {item.lectureDays}요일
-                </p>
-              </div>
-
-              <div className={styled.lecture_info}>
-                <p>
-                  <span className={styled.icon}>
-                    <IconLocation />
-                  </span>
-                  {item.lectureLocation}
-                </p>
-              </div>
-
-              <div className={styled.lecture_info}>
-                <p>
-                  <span className={styled.icon}>
-                    <IconRepeat />
-                  </span>
-                  종료일 없이 반복
-                </p>
-              </div>
-
-              <div className={styled.lecture_info}>
-                <p>
-                  <span
-                    className={styled.color_box}
-                    style={{ backgroundColor: `${item.lectureColor}` }}></span>
-                  구분색
-                </p>
-              </div>
-
-              <div className={styled.color_input}></div>
-            </div>
+      <div className={styled.invite}>
+        <h3>수강생 목록</h3>
+        <div className={styled.student}>
+          <div className={styled.count_box}>
+            <p className={styled.personnel}>현재 인원</p>
+            <p className={styled.count}>
+              {lecture.members && lecture.members.length > 0
+                ? `${lecture.members.length}명`
+                : '0명'}
+            </p>
           </div>
-          <div className={styled.button_box}>
-            <button
-              className={styled.delete}
-              onClick={() => setShowConfirmModal(true)}>
-              <IconTrashcan />
-              수업 삭제
-            </button>
-
-            {showConfirmModal && (
-              <ConfirmModal
-                isOpen={showConfirmModal}
-                onConfirm={async () => {
-                  await DeleteHandler();
-                  setShowConfirmModal(false);
-                }}
-                onCancel={() => setShowConfirmModal(false)}
-              />
-            )}
-
-            {item.memberUserId === 0 ? (
-              <></>
+          <div className={`${styled.profile} ${styled.box}`}>
+            {lecture.members && lecture.members.length > 0 ? (
+              <>
+                {lecture.members.map((member, index) => (
+                  <Image
+                    key={index}
+                    src={member.memberProfileImage}
+                    alt="회원 프로필 사진"
+                    width={32}
+                    height={32}
+                    style={{
+                      borderRadius: '32px',
+                      verticalAlign: 'middle',
+                    }}
+                  />
+                ))}
+              </>
             ) : (
-              <button className={styled.feedback_btn}>
-                수강생 전체 피드백 남기기
-              </button>
+              <>
+                <p className={styled.profile_no}>
+                  QR(수업 정보)을 공유해주세요!
+                </p>
+              </>
             )}
           </div>
-        </>
-      ))}
+        </div>
+      </div>
+
+      <div className={styled.class}>
+        <h3>수업 정보</h3>
+
+        <div className={styled.class_info}>
+          <div className={styled.class_time}>
+            <div>
+              <span className={styled.icon}>
+                <IconClock />
+              </span>
+              <span className={styled.twelve}>
+                {parseInt(lecture.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
+              </span>
+              {lecture.lectureTime[0]}
+            </div>
+            ~
+            <div>
+              <span className={styled.twelve}>
+                {parseInt(lecture.lectureTime[0], 10) >= 12 ? `PM` : `AM`}
+              </span>
+              {lecture.lectureTime[1]}
+            </div>
+          </div>
+
+          <div className={styled.lecture_info}>
+            <p>
+              <span className={styled.icon}>
+                <IconCalendar />
+              </span>
+              매주&nbsp;
+              {lecture.lectureDays.replace(/(.)(?=.)/g, '$1, ')}요일
+            </p>
+          </div>
+
+          <div className={styled.lecture_info}>
+            <p>
+              <span className={styled.icon}>
+                <IconLocation />
+              </span>
+              {lecture.lectureLocation}
+            </p>
+          </div>
+
+          <div className={styled.lecture_info}>
+            <p>
+              <span className={styled.icon}>
+                <IconRepeat />
+              </span>
+              종료일 없이 반복
+            </p>
+          </div>
+
+          <div className={styled.lecture_info}>
+            <p>
+              <span
+                className={styled.color_box}
+                style={{ backgroundColor: `${lecture.lectureColor}` }}></span>
+              구분색
+            </p>
+          </div>
+
+          <div className={styled.color_input}></div>
+        </div>
+      </div>
+      <div className={styled.button_box}>
+        <button
+          className={styled.delete}
+          onClick={() => setShowConfirmModal(true)}>
+          <IconTrashcan />
+          수업 삭제
+        </button>
+
+        {showConfirmModal && (
+          <ConfirmModal
+            isOpen={showConfirmModal}
+            onConfirm={async () => {
+              await DeleteHandler();
+              setShowConfirmModal(false);
+            }}
+            onCancel={() => setShowConfirmModal(false)}
+          />
+        )}
+
+        {lecture.memberUserId === 0 ? (
+          <></>
+        ) : (
+          <button className={styled.feedback_btn}>
+            수강생 전체 피드백 남기기
+          </button>
+        )}
+      </div>
       <div className={styled.bottom_gap}></div>
     </div>
   );
--- just-swim-app/src/app/(afterLogin)/instructor/class/edit/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/instructor/class/edit/[id]/page.tsx
@@ -11,18 +11,18 @@ import {
 } from '@components';
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import styled from './classInfoEdit.module.scss';
 
 export default function ClassInfoEdit() {
   const params = useParams();
+  const router = useRouter();
 
   const lectureId = params.id;
   const API_URL = `${process.env.NEXT_PUBLIC_DB_HOST}/api/lecture/${lectureId}`;
   const AUTHORIZATION_HEADER = `${process.env.NEXT_PUBLIC_DB_TOKEN}`;
 
   const [lecture, setLecture] = useState([]);
-  const [error, setError] = useState(null);
   const [formData, setFormData] = useState({});
 
   const handleChange = (e: { target: { name: string; value: string } }) => {
@@ -39,29 +39,20 @@ export default function ClassInfoEdit() {
         Authorization: AUTHORIZATION_HEADER,
       },
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`API error: ${response.status}`);
-        }
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data.data);
-        setLecture(data.data);
-      })
-      .catch((error) => {
-        console.error('API error:', error);
-        setError(error);
+        if (data.data && data.data.lectureTime) {
+          const lectureTime = data.data.lectureTime.split('-');
+          setLecture({ ...data.data, lectureTime });
+          console.log(data);
+        } else {
+          setLecture(data.data);
+        }
       });
-  }, []);
-
-  if (error) {
-    console.error('API error:', error);
-    return <p>API 오류 발생</p>;
-  }
+  }, [lectureId]);
 
   if (!lecture || lecture.length === 0) {
-    return <p>데이터 로딩 중...</p>;
+    return <p>로딩 중</p>;
   }
 
   const isFormDataChanged = (lectureData: object, formData: object) => {
@@ -86,129 +77,113 @@ export default function ClassInfoEdit() {
           body: JSON.stringify(formData),
         },
       );
-
       const updatedLectureData = await response.json();
       console.log(updatedLectureData);
-
       setLecture(updatedLectureData);
-
       alert('수정되었습니다.');
+      router.push(`/instructor/class/detail/${lectureId}`);
     } catch (error) {
-      console.error('API error:', error);
       alert('수정 중 오류가 발생했습니다.');
     }
   };
 
   return (
     <div>
-      {lecture && lecture.length > 0 ? (
-        lecture.map((item) => (
-          <div key={item.id}>
-            <Header title="수업 정보 수정" />
+      <div key={lecture.id}>
+        <Header title="수업 정보 수정" />
+        <div className={styled.edit}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEdit(lecture.lectureId);
+            }}>
+            <div className={styled.inner}>
+              <h3>
+                수정된 정보는 수강생 분들에게도
+                <br />
+                적용되니 유의해주세요.
+              </h3>
 
-            <div className={styled.edit}>
-              {lecture.map((item) => (
-                <>
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      handleEdit(item.lectureId);
-                    }}>
-                    <div className={styled.inner}>
-                      <h3>
-                        수정된 정보는 수강생 분들에게도
-                        <br />
-                        적용되니 유의해주세요.
-                      </h3>
+              <label htmlFor="lectureTitle">
+                수업명
+                <span>(필수)</span>
+              </label>
+              <TextInput
+                name="lectureTitle"
+                value={formData.lectureTitle || lecture.lectureTitle}
+                onChange={handleChange}
+              />
 
-                      <label htmlFor="lectureTitle">
-                        수업명
-                        <span>(필수)</span>
-                      </label>
-                      <TextInput
-                        name="lectureTitle"
-                        value={formData.lectureTitle || item.lectureTitle}
-                        onChange={handleChange}
-                      />
+              <label htmlFor="lectureContent">
+                수업 설명
+                <span>(필수)</span>
+              </label>
+              <TextInput
+                name="lectureContent"
+                value={formData.lectureContent || lecture.lectureContent}
+                onChange={handleChange}
+              />
+            </div>
 
-                      <label htmlFor="lectureContent">
-                        수업 설명
-                        <span>(필수)</span>
-                      </label>
-                      <TextInput
-                        name="lectureContent"
-                        value={formData.lectureContent || item.lectureContent}
-                        onChange={handleChange}
-                      />
-                    </div>
+            <div className={styled.line}></div>
 
-                    <div className={styled.line}></div>
+            <div className={styled.inner}>
+              <label htmlFor="lectureTime">
+                수업 시간
+                <span>(필수)</span>
+              </label>
+              <TimeInput
+                name="lectureTime"
+                defaultValue={formData.lectureTime || lecture.lectureTime}
+                defaultTimeValue={lecture.lectureTime}
+                onChange={handleChange}
+              />
 
-                    <div className={styled.inner}>
-                      <label htmlFor="lectureTime">
-                        수업 시간
-                        <span>(필수)</span>
-                      </label>
-                      <TimeInput
-                        name="lectureTime"
-                        defaultValue={formData.lectureTime || item.lectureTime}
-                        defaultTimeValue={item.lectureTime}
-                        onChange={handleChange}
-                      />
+              <label htmlFor="lectureDays">
+                수업 요일
+                <span>(필수)</span>
+              </label>
+              <DayInput
+                name="lectureDays"
+                defaultValue={formData.lectureDays || lecture.lectureDays}
+              />
 
-                      <label htmlFor="lectureDays">
-                        수업 요일
-                        <span>(필수)</span>
-                      </label>
-                      <DayInput
-                        name="lectureDays"
-                        defaultValue={formData.lectureDays || item.lectureDays}
-                      />
-
-                      <label htmlFor="lectureLocation">수업 위치</label>
-                      <TextInput
-                        name="lectureLocation"
-                        value={formData.lectureLocation || item.lectureLocation}
-                        onChange={handleChange}
-                      />
-                      {/* 수정 완료되면 추가 */}
-                      {/* <LocationInput
+              <label htmlFor="lectureLocation">수업 위치</label>
+              <TextInput
+                name="lectureLocation"
+                value={formData.lectureLocation || lecture.lectureLocation}
+                onChange={handleChange}
+              />
+              {/* 수정 완료되면 추가 */}
+              {/* <LocationInput
                   name="lectureLocation"
-                  defaultValue={item.lectureLocation}
+                  defaultValue={lecture.lectureLocation}
                 /> */}
 
-                      <label htmlFor="lectureEndDate">종료 일자</label>
-                      <DateInput
-                        name="lectureEndDate"
-                        defaultValue={
-                          formData.lectureEndDate || item.lectureEndDate
-                        }
-                        onChange={handleChange}
-                      />
+              <label htmlFor="lectureEndDate">종료 일자</label>
+              <DateInput
+                name="lectureEndDate"
+                defaultValue={formData.lectureEndDate || lecture.lectureEndDate}
+                onChange={handleChange}
+              />
 
-                      <label htmlFor="lectureColor">구분 색</label>
-                      <ColorInput
-                        name="lectureColor"
-                        defaultValue={formData.lectureColor || item.lectureColor}
-                        onChange={handleChange}
-                      />
-                    </div>
-
-                    <button
-                      className={styled.edit_btn}
-                      type="submit"
-                      disabled={!isFormDataChanged(item, formData)}>
-                      수정하기
-                    </button>
-                  </form>
-                </>
-              ))}
+              <label htmlFor="lectureColor">구분 색</label>
+              <ColorInput
+                name="lectureColor"
+                defaultValue={formData.lectureColor || lecture.lectureColor}
+                onChange={handleChange}
+              />
             </div>
-          </div>
-        ))
-      ) : (
-        <p>데이터를 불러오는 중입니다...</p>
-      )}
+
+            <button
+              className={styled.edit_btn}
+              type="submit"
+              disabled={!isFormDataChanged(lecture, formData)}>
+              수정하기
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
