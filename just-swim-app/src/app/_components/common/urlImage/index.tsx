54f883485baafb7050a2de0bb8aaeebe2ea8bfef
sdh20282 <sdh20282@gmail.com>
2024-10-19 18:30:23 +0900

    fix: /account 페이지에서 프로필 이미지가 없을 때 아무 이미지도 들어가지 않던 문제 수정

--- just-swim-app/src/app/_components/common/urlImage/index.tsx
+++ just-swim-app/src/app/_components/common/urlImage/index.tsx
@@ -9,7 +9,7 @@ export const URLImage = (props: URLImageProps) => {
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
       <Image
-        src={props.imageURL}
+        src={props.imageURL || '/assets/profile1.png'}
         alt={props.alt}
         fill={true}
         object-fit="cover"
