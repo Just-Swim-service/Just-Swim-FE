273f2aa292937063098f72e9fea3f69692dc08b1
jae-hwan-kim <woghks7859@gmail.com>
2025-03-20 21:56:10 +0900

    fix: 스케줄 탭, type 정보 가저오는 방법 api호출로 수정

--- just-swim-app/src/app/(afterLogin)/schedule/(schedule)/_components/scheduleAddButton/index.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/(schedule)/_components/scheduleAddButton/index.tsx
@@ -2,48 +2,36 @@
 
 import styled from './styles.module.scss';
 import Link from 'next/link';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IconAdd } from '@assets';
-import { useUserStore } from '@store';
-import { getMyProfile } from '@apis';
+import { getCachedMyProfile } from '@apis';
 import { useRouter } from 'next/navigation';
-import { get, isEmpty, trim } from 'lodash';
+import { isEmpty } from 'lodash';
+
+type ProfileInfo = {
+  name: string;
+  profileImage: string;
+  userType: string;
+};
 
 export function ScheduleAddButton({ token }: { token: string }) {
   const router = useRouter();
-  const { getUserType, getUser, setAddUserProfile, setAddUserToken } =
-    useUserStore();
-  const [type, setType] = useState<string>('');
-
-  const setUserData = useCallback(async () => {
-    try {
-      const { status, data } = await getMyProfile();
-      if (status === 406) {
-        setAddUserToken('');
-        return router.replace('/signin');
-      }
-      setAddUserProfile({ token: token, profile: data?.data });
-    } catch (error) {
-      setAddUserToken('');
-      return router.replace('/signin');
-    }
-  }, [setAddUserProfile, setAddUserToken, token, router]);
+  const [profileInfo, setProfileInfo] = useState<ProfileInfo>();
 
   useEffect(() => {
-    const user = getUser();
-
-    if (Object.keys(user).length === 0 || !user) {
-      setUserData().then(() => {
-        setType(trim(getUserType(token)));
-      });
-    } else {
-      setType(trim(getUserType(token)));
-    }
-  }, [getUser, getUserType, setUserData, token]);
+    const fetchProfileInfo = async () => {
+      const response = await getCachedMyProfile();
+      if (isEmpty(response)) {
+        return router.replace('/signin');
+      }
+      setProfileInfo(response as ProfileInfo);
+    };
+    fetchProfileInfo();
+  }, [router]);
 
   return (
     <>
-      {type === 'instructor' && (
+      {profileInfo?.userType === 'instructor' && (
         <Link href="/schedule/add" className={styled.link}>
           <IconAdd />
         </Link>
