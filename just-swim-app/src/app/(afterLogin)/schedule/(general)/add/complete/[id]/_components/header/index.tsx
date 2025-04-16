cab7fbe104490e05e36753d6d4cb65316b09a954
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-04-16 11:15:07 +0900

    fix: schedule/add/complete 헤더 변경

--- just-swim-app/src/app/(afterLogin)/schedule/(general)/add/complete/[id]/_components/header/index.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/(general)/add/complete/[id]/_components/header/index.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { HistoryBackHeader } from "@components";
+import styled from './styled.module.scss';
 
 export function QRCodeHeader() {
   return (
-    <HistoryBackHeader title='수업 등록하기' />
-  )
+    <header className={styled.header}>
+      <div className={styled.title_wrapper}>
+        <h1>수업 등록 완료</h1>
+      </div>
+    </header>
+  );
 }
