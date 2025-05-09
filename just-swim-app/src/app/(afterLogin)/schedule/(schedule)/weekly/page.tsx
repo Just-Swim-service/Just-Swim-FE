6279ca93f6bec4c0480c0229c161c2c3db2c5825
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-09 13:40:41 +0900

    fix: weekly 부분에서 cache 제거

--- just-swim-app/src/app/(afterLogin)/schedule/(schedule)/weekly/page.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/(schedule)/weekly/page.tsx
@@ -1,8 +1,8 @@
-import { getCachedWeeklyScheduleInfo } from '@utils';
+import { getWeeklyScheduleInfo } from '@utils';
 import { WeekWrapper } from './_components';
 
 export default async function Weekly() {
-  const weeklyInfo = await getCachedWeeklyScheduleInfo();
+  const weeklyInfo = await getWeeklyScheduleInfo();
 
   return <WeekWrapper weeklyInfo={weeklyInfo} />;
 }
