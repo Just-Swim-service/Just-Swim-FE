6935f290f1c5cb70504e8b3042f82674e0d8d146
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-16 15:57:08 +0900

    fix: schedule page.tsx 수정

--- just-swim-app/src/app/(afterLogin)/schedule/(schedule)/page.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/(schedule)/page.tsx
@@ -1,12 +1,16 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Page() {
   const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
-    router.replace('/schedule/weekly');
-  }, [router]);
+    if (pathname !== '/schedule/weekly') {
+      router.replace('/schedule/weekly');
+    }
+  }, [router, pathname]);
 
   return <div>Redirecting...</div>;
 }
