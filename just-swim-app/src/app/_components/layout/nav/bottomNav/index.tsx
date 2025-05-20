c314dfd732233235523821921a916d1a8aa76e9d
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-20 15:12:43 +0900

    fix: bottomNav를 고려한 수정

--- just-swim-app/src/app/_components/layout/nav/bottomNav/index.tsx
+++ just-swim-app/src/app/_components/layout/nav/bottomNav/index.tsx
@@ -3,40 +3,38 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-import {
-  IconCalendarHome,
-  IconClass,
-  IconFeedback,
-} from '@assets';
+import { IconCalendarHome, IconClass, IconFeedback } from '@assets';
 
 import styled from './styles.module.scss';
 
 export function BottomNav() {
   const pathname = usePathname();
+  const isMonthly = pathname === '/schedule/monthly';
 
   return (
-    <div className={styled.container}>
+    <div
+      className={`${styled.container} ${isMonthly ? styled.static : styled.fixed}`}>
       <div className={styled.list}>
         <Link
           href="/schedule"
           className={`${styled.item} ${pathname.startsWith('/schedule') && styled.selected}`}>
-            <IconCalendarHome width={22} height={22} />
-            <span>홈</span>
+          <IconCalendarHome width={22} height={22} />
+          <span>홈</span>
         </Link>
         <Link
           href="/class"
           className={`${styled.item} ${pathname.startsWith('/class') && styled.selected}`}>
-            <IconClass width={22} height={22} />
-            <span>수업</span>
+          <IconClass width={22} height={22} />
+          <span>수업</span>
         </Link>
         <Link
           href="/feedback"
           className={`${styled.item} ${pathname.startsWith('/feedback') && styled.selected}`}>
-            <IconFeedback width={22} height={22} />
-            <span>피드백</span>
+          <IconFeedback width={22} height={22} />
+          <span>피드백</span>
         </Link>
       </div>
       <div className={styled.divider} />
     </div>
-  )
+  );
 }
