ec0d9113b5eb2920faf990071877f82364ef462c
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-28 14:24:19 +0900

    fix: schedule layout에 맞춰 수정

--- just-swim-app/src/app/(afterLogin)/schedule/(schedule)/_components/scheduleCommonLayout/index.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/(schedule)/_components/scheduleCommonLayout/index.tsx
@@ -5,18 +5,19 @@ import { getTodayScheduleCount } from '@utils';
 import { ScheduleCommon } from '../scheduleCommon';
 import { useEffect, useState } from 'react';
 import { ScheduleCommonSkeleton } from '../skeleton';
+import { usePathname } from 'next/navigation';
 
 export function ScheduleCommonLayout() {
+  const pathname = usePathname();
   const [todayCount, setTodayCount] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchTodayCount = async () => {
       const todayCount = await getTodayScheduleCount();
-      console.log('🎯 todayCount (client):', todayCount);
       setTodayCount(todayCount);
     };
     fetchTodayCount();
-  }, []);
+  }, [pathname]);
 
   if (todayCount === null) return <ScheduleCommonSkeleton />;
 
