888f40bc3cc0fdde6ce267edd2931401c896029f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-05-30 17:09:23 +0900

    fix: 진입 페이지 수정

--- just-swim-app/src/app/page.tsx
+++ just-swim-app/src/app/page.tsx
@@ -1,6 +1,15 @@
 import { redirect } from 'next/navigation';
 import { ROUTES } from '@data';
+import { cookies } from 'next/headers';
 
 export default function Page() {
-  redirect(ROUTES.ONBOARDING.signin);
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('authorization')?.value;
+  const refreshToken = cookieStore.get('refreshToken')?.value;
+
+  if (accessToken && refreshToken) {
+    redirect(ROUTES.SCHEDULE.root);
+  } else {
+    redirect(ROUTES.ONBOARDING.signin);
+  }
 }
