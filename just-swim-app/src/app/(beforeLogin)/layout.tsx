49e71fd7fb37f9c8ad4a62ce6cedd696a35bf21f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-06-02 16:03:04 +0900

    fix: signin 페이지 refresh 토큰 처리 추가

--- just-swim-app/src/app/(beforeLogin)/layout.tsx
+++ just-swim-app/src/app/(beforeLogin)/layout.tsx
@@ -1,5 +1,33 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './layout.module.scss';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkRefreshToken = async () => {
+      try {
+        const res = await fetch('/auth/refresh', {
+          method: 'POST',
+          credentials: 'include',
+        });
+
+        if (res.ok) {
+          console.log('✅ Refresh 성공, /schedule 이동');
+          router.replace('/schedule');
+        } else {
+          console.log('❌ Refresh 실패, 로그인 폼 유지');
+        }
+      } catch (err) {
+        console.error('Silent Refresh 에러:', err);
+      }
+    };
+
+    checkRefreshToken();
+  }, [router]);
+
   return <div className={styles.before_login_container}>{children}</div>;
 }
