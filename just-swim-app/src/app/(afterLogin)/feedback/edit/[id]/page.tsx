3ddcca3e97dd07e3ae9c450270bcc978418a25a2
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-06-08 13:01:57 +0900

    fix: feedback/create/person 페이지 페이지 이동에 맞춰 값이 남겨져있게 수정

--- just-swim-app/src/app/(afterLogin)/feedback/edit/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/feedback/edit/[id]/page.tsx
@@ -9,7 +9,7 @@ import { IconArrowRightSmall } from '@assets';
 
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Header } from '@components';
 import { FeedbackInfo, Members } from '@/_types/typeFeedback';
 import { DateInput, FileInput, LinkInput, TextArea } from '@components';
@@ -43,6 +43,7 @@ export default function FeedbackInfoEdit() {
     register,
     setValue,
     reset,
+    control,
     handleSubmit,
     formState: { errors, isValid, isDirty },
   } = useForm<FormType>({
@@ -190,25 +191,36 @@ export default function FeedbackInfoEdit() {
               <div className={styled.title}>
                 피드백 기준 수업일 <span>(필수)</span>
               </div>
-              <DateInput
-                renderIcon={() => <IconCalendar width={14} height={14} />}
-                placeholder="수업 일자를 선택해주세요"
-                {...register('date')}
-                defaultValue={feedback?.feedbackDate}
-                // @ts-ignore
-                errors={[errors.date?.message ?? '']}
+              <Controller
+                name="date"
+                control={control}
+                render={({ field }) => (
+                  <DateInput
+                    name="date"
+                    renderIcon={() => <IconCalendar width={14} height={14} />}
+                    placeholder="수업 일자를 선택해주세요"
+                    defaultValue={field.value}
+                    onChange={(value) => field.onChange(value)}
+                  />
+                )}
               />
             </div>
 
             <div className={styled.wrap}>
               <div className={styled.title}>첨부 파일</div>
-              <FileInput
-                {...register('file')}
-                defaultImages={
-                  feedback?.images?.map((img) => img.imagePath) || []
-                }
-                // @ts-ignore
-                setValue={setValue}
+              <Controller
+                name="file"
+                control={control}
+                render={({ field }) => (
+                  <FileInput
+                    name="file"
+                    defaultImages={
+                      feedback?.images?.map((img) => img.imagePath) || []
+                    }
+                    onChange={(newFiles) => field.onChange(newFiles)}
+                    setValue={setValue}
+                  />
+                )}
               />
             </div>
 
