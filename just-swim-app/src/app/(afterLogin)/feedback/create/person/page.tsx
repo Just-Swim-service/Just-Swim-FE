24f04d18a992d23e455c7304b577edb9e47d8b37
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-14 14:14:21 +0900

    fix: content type 부분 추가

--- just-swim-app/src/app/(afterLogin)/feedback/create/person/page.tsx
+++ just-swim-app/src/app/(afterLogin)/feedback/create/person/page.tsx
@@ -147,6 +147,9 @@ export default function FeedbackWrite() {
           const response = await fetch(presignedUrl, {
             method: 'PUT',
             body: file,
+            headers: {
+              'Content-Type': file.type,
+            },
           });
 
           if (!response.ok) throw new Error('파일 업로드 실패');
