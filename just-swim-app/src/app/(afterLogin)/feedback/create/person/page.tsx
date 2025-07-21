79702acf78c32d6f4548f5918dfa686f0adedf70
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-21 13:44:06 +0900

    fix: fileName 수정

--- just-swim-app/src/app/(afterLogin)/feedback/create/person/page.tsx
+++ just-swim-app/src/app/(afterLogin)/feedback/create/person/page.tsx
@@ -120,11 +120,7 @@ export default function FeedbackWrite() {
         if (!file) return null;
 
         try {
-          const safeFileName = file.name
-            .normalize('NFKD')
-            .replace(/[^\w.-]/g, '_');
-
-          const [presigned] = await getFeedbackPresignedURL([safeFileName]);
+          const [presigned] = await getFeedbackPresignedURL([file.name]);
           const { presignedUrl, contentType } = presigned;
 
           const response = await fetch(presignedUrl, {
