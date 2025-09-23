5b2d82d624e4acc544eda2bd4d67423bdab4568b
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-09-23 15:55:10 +0900

    fix: 로그아웃 기능 수정

--- just-swim-app/src/app/_utils/token.ts
+++ just-swim-app/src/app/_utils/token.ts
@@ -16,40 +16,14 @@ export const getTokenInCookies = () => {
 };
 
 export const removeTokenInCookies = () => {
-  // 서버 사이드에서 쿠키 삭제 - BE와 동일한 옵션 사용
-  cookies().set('authorization', '', {
-    expires: new Date(0),
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    domain: '.just-swim.kr',
-    path: '/',
-  });
-
-  cookies().set('refreshToken', '', {
-    expires: new Date(0),
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    domain: '.just-swim.kr',
-    path: '/',
-  });
-
+  // 서버 사이드에서 쿠키 직접 삭제 - BE와 동일한 옵션 사용
+  cookies().delete('authorization');
+  cookies().delete('refreshToken');
   return;
 };
 
-// 클라이언트 사이드에서 쿠키 삭제 - BE와 동일한 옵션 사용
+// 클라이언트 사이드에서 쿠키 삭제
 export const removeTokenInCookiesClient = () => {
-  Cookies.remove('authorization', {
-    domain: '.just-swim.kr',
-    path: '/',
-    secure: true,
-    sameSite: 'none',
-  });
-  Cookies.remove('refreshToken', {
-    domain: '.just-swim.kr',
-    path: '/',
-    secure: true,
-    sameSite: 'none',
-  });
+  Cookies.remove('authorization');
+  Cookies.remove('refreshToken');
 };
