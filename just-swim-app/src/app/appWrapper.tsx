e4871722051de1a9526f7055a07505a8f19bf106
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-10-01 15:58:31 +0900

    fix: community에 formBody 디자인 수정

--- just-swim-app/src/app/appWrapper.tsx
+++ just-swim-app/src/app/appWrapper.tsx
@@ -5,9 +5,11 @@ import { usePathname } from 'next/navigation';
 export function AppWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isSchedule = pathname.startsWith('/schedule');
+  const isCommunityCreate = pathname.startsWith('/community/create');
+  const shouldApplyAppLayout = !isSchedule && !isCommunityCreate;
 
   return (
-    <div className={isSchedule ? '' : 'app_layout'}>
+    <div className={shouldApplyAppLayout ? 'app_layout' : ''}>
       {children}
       <div id="modal-portal" />
       <div id="toast-portal" />
