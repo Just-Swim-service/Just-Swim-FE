0a82d0edda09e1f7af6ac2089ee8f24996b4cb2f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-10-13 15:08:52 +0900

    fix: kebab 메뉴 디자인 수정

--- just-swim-app/src/app/(afterLogin)/community/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/community/[id]/page.tsx
@@ -427,6 +427,33 @@ export default function CommunityDetailPage() {
                   <IconKebabMenu width={20} height={20} fill="#666" />
                 </button>
 
+                {/* 간단한 테스트 */}
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '100%',
+                    right: '0',
+                    zIndex: 999,
+                    backgroundColor: 'red',
+                    color: 'white',
+                    padding: '10px',
+                    border: '2px solid black',
+                    marginTop: '4px',
+                  }}>
+                  테스트 드롭다운
+                </div>
+
+                {/* 더 간단한 테스트 */}
+                <div
+                  style={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    padding: '5px',
+                    marginTop: '10px',
+                  }}>
+                  간단한 테스트
+                </div>
+
                 {/* 항상 보이는 테스트 모달 */}
                 <DropdownModal
                   isOpen={true}
