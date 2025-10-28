fbf3d5ae56886fb1c9066965a1fa8b5f3b40cb0f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-10-28 14:35:59 +0900

    fix: 북마크 토글 디자인 수정

--- just-swim-app/src/app/(afterLogin)/community/page.tsx
+++ just-swim-app/src/app/(afterLogin)/community/page.tsx
@@ -313,11 +313,7 @@ export default function CommunityPage() {
                 className={`${styled.bookmarkToggle} ${showBookmarkOnly ? styled.active : ''}`}
                 onClick={() => setShowBookmarkOnly(!showBookmarkOnly)}
                 title="북마크한 글">
-                {showBookmarkOnly ? (
-                  <IconBookmarkFilled width={20} height={20} />
-                ) : (
-                  <IconBookmark width={20} height={20} />
-                )}
+                {showBookmarkOnly ? <IconBookmarkFilled /> : <IconBookmark />}
               </button>
             </div>
 
